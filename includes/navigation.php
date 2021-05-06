4ac6e5202a68aa704497b41871d7da1b444825a3
jrjakiro <jrjakiro2001@gmail.com>
2021-05-06 13:47:13 +0530

    New Header is Generated by JR on 6 May

---
+++ includes/navigation.php
@@ -0,0 +1,34 @@
+<?php
+	include_once "../info.php";
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+	<link rel="stylesheet" href="../css/includes/navigation.css">
+</head>
+<body>
+	<header id="header">
+		<img src="../images/logo.png" alt= "logo" id="header-img">
+		<h1>EDUHUB</h1>
+		<div class="user-details">
+			<span><i class="fa fa-plus"></i></span>
+			<span><i class="fa fa-bell"></i></span>
+			<span><i class="fa fa-power-off"></i></span>
+		</div>
+		<nav id="nav-bar">
+			<ul>
+				<li><a class="nav-link" href="">Home</a></li>
+				<li><a class="nav-link" href="">Explore</a></li>
+				<li><a class="nav-link" href="">Profile</a></li>
+				<li><a class="nav-link" href="">settings</a></li>
+				<li><a class="nav-link" href="">Help</a></li>
+				<li><a class="nav-link" href="">about</a></li>
+			</ul>
+			<button><span></span>Search</button>
+		</nav>
+	</header>
+
+  <script  src="../javascript/includes/navigation.js"></script>
+
+</body>
+</html>
