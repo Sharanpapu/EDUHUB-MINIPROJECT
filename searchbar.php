795608c8fa217b5d9388769d875b46ebcb4a5fc7
shreesharanyaa <sec19it156@sairamtap.edu.in>
2021-06-16 10:52:31 +0530

    added searchbar

---
+++ searchbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Search Bar</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/search.css">
+
+</head>
+<body>
+<form action="search.php" method="POST"> 
+<!-- partial:index.partial.html -->
+<div class="container">
+  <input type="text" placeholder="Search..." name="query">
+  <div class="search"></div>
+</div>
+<!-- partial -->
+  
+
+
+
+</form>
+</body>
+</html>
